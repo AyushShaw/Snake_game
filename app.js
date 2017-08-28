ef9f228da9853d6f0667914a5060f6b9aeb30370
Ayush Shaw <ayushshaw@outlook.com>
2017-08-28 07:20:06 +0530

    The Snake Grows now
    Still need to make the Eat attribute for Sanke function

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function setup(){
     createCanvas(600,600);
     snake = new Snake();
     food = new Food();
-    frameRate(9);
+    frameRate(7);
     food.picklocation();
 }
 
@@ -17,6 +17,10 @@ function draw(){
     food.Fshow();
     
 }
+
+function mousePressed() {
+    snake.total++;
+  }
 
 function keyPressed(){
 
@@ -45,22 +49,42 @@ function Snake(){
     this.y = 0;
     this.xspeed = 1;
     this.yspeed = 0;
-    
+    this.total = 0;
+    this.snk = [];
+
     this.dir =  function(x, y ){
         this.xspeed = x;
         this.yspeed = y;
     }
 
+
+
+
     this.update = function(){
+
+        for(var i = 0; i< this.snk.length-1; i++){
+            this.snk[i] = this.snk[i+1];
+            
+        }
+        if(this.total >= 1){
+            /* for(var i = this.total -1; i> 0 || i == 0; i--){*/
+                this.snk[this.total -1] = createVector(this.x, this.y);
+            
+        }
         this.x = this.x + this.xspeed*S;
         this.y = this.y + this.yspeed*S;
-
+        
+        
         this.x = constrain(this.x, 0, width-S);
         this.y = constrain(this.y, 0, height-S);
     }
 
     this.show = function(){
         fill(255);
+        for (var index = 0; index < this.snk.length; index++) {
+            rect(this.snk[index].x, this.snk[index].y, S, S);
+            
+        }
         rect(this.x, this.y, S, S);
     }
 }
@@ -74,10 +98,10 @@ function Food() {
         F = createVector(floor(random(col)), floor(random(row)));
         F.mult(S);
     }
-    
+
     this.Fshow  = function(){
         fill(255, 0, 100,);
         rect(F.x,F.y , S, S);
     }
-
+    
 }
